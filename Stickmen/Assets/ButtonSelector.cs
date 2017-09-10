d14916d7ee6208646ca52bcc6e84557389e6aa2d
Jérémie Jasmin <jeremiejasmin1@hotmail.com>
2017-09-10 18:04:58 -0400

    debut du projet unity

---
+++ Stickmen/Assets/ButtonSelector.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ButtonSelector : MonoBehaviour {
+
+    private Collider _collider;
+    private Material _material;
+    private TextMeshPro _textMesh;
+
+    public Color baseColor;
+    public Color clicColor;
+
+	// Use this for initialization
+	void Awake () {
+        _collider = GetComponent<BoxCollider>();
+        _material = GetComponent<MeshRenderer>().material;
+        _textMesh = GetComponentInChildren<TextMeshPro>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnMouseEnter()
+    {
+        _textMesh.fontSharedMaterial.SetFloat(ShaderUtilities.ID_GlowOuter, 1f);
+    }
+
+    private void OnMouseExit()
+    {
+        _textMesh.fontSharedMaterial.SetFloat(ShaderUtilities.ID_GlowOuter, 0f);
+    }
+
+    private void OnMouseDown()
+    {
+        _material.color = clicColor;
+    }
+
+    private void OnMouseUp()
+    {
+        _material.color = baseColor;
+    }
+}
